06ca5b0c7eb7cc526debd1b8bedbe0a733b1dff8
Aris <wkx5kl@gmail.com>
2024-10-08 17:52:41 +0800

    update note for 2024-10-08 4

---
+++ contracts/26_DeployContract.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+import "./26_DeleteContract.sol";
+
+contract DeployContract {
+    struct DemoResult {
+        address addr;
+        uint balance;
+        uint value;
+    }
+
+    constructor() payable {}
+
+    function getBalance() external view returns (uint balance) {
+        balance = address(this).balance;
+    }
+
+    function demo() public payable returns (DemoResult memory) {
+        DeleteContract del = new DeleteContract{value: msg.value}();
+        DemoResult memory res = DemoResult({
+            addr: address(del),
+            balance: del.getBalance(),
+            value: del.value()
+        });
+        del.deleteContract();
+        return res;
+    }
+}
